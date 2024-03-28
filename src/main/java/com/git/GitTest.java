932729c331f4c2ab97d9e4d0ea1f9e351b9a3397
gky <3136275913@qq.com>
2024-03-28 17:10:11 +0800

    hot-fix first commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello,git");
         System.out.println("hello,git again");
         System.out.println("hello,git again again");
+        System.out.println("hello,hot-fix");
     }
 }
