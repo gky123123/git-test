cb47e7ae42d5cd780e82354bad770cdcc35ef284
gky <3136275913@qq.com>
2024-03-28 17:12:32 +0800

    hot-fix second commit

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git");
         System.out.println("hello,git again");
+        System.out.println("hello,conflict merge");
         System.out.println("hello,git again again");
         System.out.println("hello,hot-fix");
     }
